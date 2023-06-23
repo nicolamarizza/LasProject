8ff0c844d8deb70d051a0c7b69cd6e4cf54eb29b
Nicola Marizza <nicola.marizza@gmail.com>
2023-06-24 00:45:57 +0200

    UID property set to full principal name
    1) gssapi feeds the full principal name to mod_authnz_ldap
    2) mod_authnz_ldap compares it against the uid property

--- kdc.servers.mycompany.lan/KerberosLdapBackend/addprinc.sh
+++ kdc.servers.mycompany.lan/KerberosLdapBackend/addprinc.sh
@@ -1,29 +1,50 @@
 #!/bin/bash -xe
 
-PRINC_NAME=$1
-PRINC_DEP=$2
-PRINC_PWD=$3
-BASE_DN=cn=MYCOMPANY.LAN,cn=krbContainer,dc=mycompany,dc=lan
+PRINC_PWD=$1
+PRINC_NAME=$2
+PRINC_INSTANCE=$3
+REALM=MYCOMPANY.LAN
+PRINC_UID="$PRINC_NAME/$PRINC_INSTANCE@$REALM"
+BASE_DN="cn=$REALM,cn=krbContainer,dc=mycompany,dc=lan"
+
+if [ -z "$LDAP_MASTER_PWD" ]; then
+    echo '$LDAP_MASTER_PWD variable required'
+    exit 1
+fi
 
 if [ $# -ne 3 ]; then
     echo "wrong argument number"
-    exit
+    exit 1
 fi
 
 echo -ne "Add principal to the DIT? [y|n]"
-read add
+read ADD_TO_DIT
 
-if [ $add == "y" ]; then
+if [ $# -eq 3 ]; then
+    echo -ne "put principal under ou=$PRINC_INSTANCE? [y|n]"
+    read INSTANCE_IS_OU;
+fi
 
-    ldapadd -x -H ldapi:/// -D "cn=admin,dc=mycompany,dc=lan" -w $LDAP_MASTER_PWD <<EOF
-dn: cn=$PRINC_NAME,ou=$PRINC_DEP,$BASE_DN
+if [ "$INSTANCE_IS_OU" -eq "y" ]; then
+    PRINC_DN="cn=$PRINC_NAME,ou=$PRINC_INSTANCE,$BASE_DN"
+else
+    PRINC_DN="cn=$PRINC_NAME,$BASE_DN"
+fi
+
+
+if [ "$ADD_TO_DIT" == "y" ]; then
+
+    ldapadd -x -H ldapi:/// -D "cn=admin,dc=mycompany,dc=lan" -w "$LDAP_MASTER_PWD" <<EOF
+dn: $PRINC_DN
 objectClass: person
 objectClass: organizationalPerson
 objectClass: inetOrgPerson
 cn: $PRINC_NAME
-sn: $PRINC_DEP
+sn: $PRINC_INSTANCE
+uid: $PRINC_UID
 
 EOF
 
 fi
-kadmin.local -q "addprinc -pw \"$PRINC_PWD\" -x dn=cn=$PRINC_NAME,ou=$PRINC_DEP,$BASE_DN $PRINC_NAME/$PRINC_DEP@MYCOMPANY.LAN"
+
+kadmin.local -q "addprinc -pw \"$PRINC_PWD\" -x dn=$PRINC_DN $PRINC_UID"
