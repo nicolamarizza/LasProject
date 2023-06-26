8c98d7d6b9e714b25189a7315a0933bcf4800fce
Nicola Marizza <nicola.marizza@gmail.com>
2023-06-26 19:20:31 +0200

    mod_authnz_ldap uses employee group attribute

--- kdc.servers.mycompany.lan/KerberosLdapBackend/addprinc.sh
+++ kdc.servers.mycompany.lan/KerberosLdapBackend/addprinc.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 PRINC_PWD=$1
-PRINC_NAME=$2
+PRINC_MAIN=$2
 PRINC_INSTANCE=$3
-REALM=MYCOMPANY.LAN
-PRINC_UID="$PRINC_NAME/$PRINC_INSTANCE@$REALM"
+REALM="MYCOMPANY.LAN"
+PRINC_UID="$PRINC_MAIN/$PRINC_INSTANCE@$REALM"
 BASE_DN="cn=$REALM,cn=krbContainer,dc=mycompany,dc=lan"
+MAIN_KEYTAB="/etc/krb5.keytab"
 
 if [ -z "$LDAP_MASTER_PWD" ]; then
     echo '$LDAP_MASTER_PWD variable required'
@@ -17,34 +18,47 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-echo -ne "Add principal to the DIT? [y|n]"
-read ADD_TO_DIT
+echo -ne "Is this principal an employee? y|[n] "
+read principal_is_employee;
 
-if [ $# -eq 3 ]; then
-    echo -ne "put principal under ou=$PRINC_INSTANCE? [y|n]"
-    read INSTANCE_IS_OU;
-fi
+echo -ne "Add principal to the DIT? y|[n] "
+read add_to_dit
 
-if [ "$INSTANCE_IS_OU" -eq "y" ]; then
-    PRINC_DN="cn=$PRINC_NAME,ou=$PRINC_INSTANCE,$BASE_DN"
+if [ "$principal_is_employee" == "y" ]; then
+    princ_dn="cn=$PRINC_MAIN,ou=$PRINC_INSTANCE,$BASE_DN"
 else
-    PRINC_DN="cn=$PRINC_NAME,$BASE_DN"
+    princ_dn="cn=$PRINC_MAIN,$BASE_DN"
 fi
 
 
-if [ "$ADD_TO_DIT" == "y" ]; then
+if [ "$add_to_dit" == "y" ]; then
 
     ldapadd -x -H ldapi:/// -D "cn=admin,dc=mycompany,dc=lan" -w "$LDAP_MASTER_PWD" <<EOF
-dn: $PRINC_DN
+dn: $princ_dn
 objectClass: person
 objectClass: organizationalPerson
 objectClass: inetOrgPerson
-cn: $PRINC_NAME
+cn: $PRINC_MAIN
 sn: $PRINC_INSTANCE
 uid: $PRINC_UID
 
 EOF
 
+    if [ "$principal_is_employee" == "y" ]; then
+
+    ldapmodify -x -H ldapi:/// -D "cn=admin,dc=mycompany,dc=lan" -w "$LDAP_MASTER_PWD" <<EOF
+dn: ou=$PRINC_INSTANCE,$BASE_DN
+changeType: modify
+add: employee
+employee: $princ_dn
+EOF
+
+    fi
 fi
 
-kadmin.local -q "addprinc -pw \"$PRINC_PWD\" -x dn=$PRINC_DN $PRINC_UID"
+
+temp_keytab="./${PRINC_MAIN}_${PRINC_INSTANCE}.keytab"
+
+kadmin.local -q "addprinc -pw \"$PRINC_PWD\" -x dn=$princ_dn $PRINC_UID"
+kadmin.local -q "ktadd -norandkey -k \"$temp_keytab\" \"$PRINC_MAIN/$PRINC_INSTANCE@$REALM\""
+echo -e "rkt \"$temp_keytab\"\nwkt \"$MAIN_KEYTAB\"" | ktutil
