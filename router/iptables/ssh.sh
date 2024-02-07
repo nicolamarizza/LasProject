5f57ea69ae2e3a16c6d8b410a76984a2157af669
Nicola Marizza <nicola.marizza@gmail.com>
2024-02-07 22:09:03 +0100

    Fixed iptables compatibility bugs with network update
    loop goes from 1 to 3 in ssh.sh
    ext_iface hardcoded to ens0 in variables.sh

--- router/iptables/ssh.sh
+++ router/iptables/ssh.sh
@@ -9,7 +9,7 @@ ruleAndLog "-A OUTPUT -p tcp --sport 22 -o $ext_iface" "TEST sshd out" "TEST all
 # allow ssh client requests to wan
 ruleAndLog "-A OUTPUT -p tcp --dport 22 -o $ext_iface" "TEST sshc out" "TEST allow ssh requests to wan"
 
-for ((i=3; i <= 5; i++)); do
+for ((i=1; i <= 3; i++)); do
 
 	# NETMAP inbound ssh req of type 192.168.XX.0/24 -> 192.168.X.0/24
 	ruleAndLog "-t nat -A PREROUTING -p tcp --dport 22 -d 192.168.$i$i.0/24" \
