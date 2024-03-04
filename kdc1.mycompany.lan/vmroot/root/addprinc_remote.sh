14d5c4b7d283faf5c56ecac9f494d05eb5896a74
Nicola Marizza <nicola.marizza@gmail.com>
2024-03-04 17:35:48 +0100

    addprinc_remote no longer edits /etc/auto*

--- kdc1.mycompany.lan/vmroot/root/addprinc_remote.sh
+++ kdc1.mycompany.lan/vmroot/root/addprinc_remote.sh
@@ -11,18 +11,4 @@ if [ -z $(getent group $USER_GID) ]; then
     echo "created $PRINC_DEP group on remote host"
 fi
 
-if [ $(apt list --installed 2>/dev/null | grep -E '^autofs|nfs-common'| wc -l) -lt 2 ] ; then
-    apt update
-    apt install nfs-common autofs
-fi
-
-if [ -z "$(grep -E '^/home /etc/auto.home' /etc/auto.master)" ]; then
-    echo "/home /etc/auto.home" >> /etc/auto.master
-fi
-
-if [ -z "(grep -E 'homes.mycompany.lan:/homes/$PRINC_DEP/&$')" ]; then
-    echo -e "*\t-fstype=nfs4,rw\thomes.mycompany.lan:/homes/$PRINC_DEP/&" >> /etc/auto.home
-    systemctl restart autofs
-fi
-
 useradd --shell /bin/bash --password "$PRINC_PWD" --uid $USER_UID --gid $USER_GID "$PRINC_NAME"
