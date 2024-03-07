25e4a4d8f537b6eb86441e6602770c9aac65bc13
Nicola Marizza <nicola.marizza@gmail.com>
2024-03-07 16:06:17 +0100

    Updated wsinit

--- kdc1.mycompany.lan/root/utilities/wsinit/wsinit.sh
+++ kdc1.mycompany.lan/root/utilities/wsinit/wsinit.sh
@@ -9,7 +9,7 @@ WS_IP=ws$WS_NO.$DEP.mycompany.lan
 
 DEP=$(echo $WS_IP | cut -d. -f2)
 
-ssh root@$WS_IP "bash -s" -- $DEP < ./init_remote.sh
+echo "apt update && apt install -y nfs-common autofs krb5-user libpam-krb5 chrony" | ssh root@$WS_IP "bash -s"  
 
 TEMP_DIR=$(mktemp -d)
 cp -r ./etc $TEMP_DIR/
@@ -17,5 +17,7 @@ cp -r ./etc $TEMP_DIR/
 find $TEMP_DIR -type f -exec sed -i "s/WS_DEP/$DEP/g" {} \;
 
 scp -r $TEMP_DIR/etc root@$WS_IP:/
+
+ssh root@$WS_IP systemctl restart autofs chrony
 
 rm -r $TEMP_DIR
