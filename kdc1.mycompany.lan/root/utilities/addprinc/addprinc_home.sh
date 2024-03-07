1a441b0ebb8a9e85579c65a3aa5d69f160a83b66
Nicola Marizza <nicola.marizza@gmail.com>
2024-03-07 22:00:17 +0100

    Added quota

--- kdc1.mycompany.lan/root/utilities/addprinc/addprinc_home.sh
+++ kdc1.mycompany.lan/root/utilities/addprinc/addprinc_home.sh
@@ -11,4 +11,8 @@ mkdir -p "$HOME_SHARE"
 chmod 700 "$HOME_SHARE"
 chown $USER_UID:$USER_GID "$HOME_SHARE"
 
+# 25GiB: 6553600 blocks
+# 90% of 25GiB: 5898240 blocks
+setquota -u $USER_UID 5898240 6553600 0 0 /homes/$PRINC_DEP
+
 echo "created home share $HOME_SHARE"
